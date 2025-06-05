c422e8fb33aa2713e42684a8c47dde0b965f3cfb
CarlosDanielSantana <carlosxxx7z2006@gmail.com>
2025-06-05 16:51:34 -0300

    aprendendo array

--- logicacomjs.js
+++ logicacomjs.js
@@ -21,10 +21,64 @@ let Multiplicacao = 75;
 let quantidadeParcelas = 6;
 let Divida = Multiplicacao * quantidadeParcelas;
 
-console.log("A multiplicação pela quantidade de parcelas resultou em: " + Divida + " R$");*/
+console.log("A multiplicação pela quantidade de parcelas resultou em: " + Divida + " R$");
 
 let nome_do_usuario = "Ryan";
 let salario = 2500;
 let meses_trad = 8
 let decimo = salario/12*meses_trad
 console.log(nome_do_usuario+"seu decimo é: "+ decimo+ " parabems")
+
+let valordobasico = 25;
+let valordopremium = 40;
+let valordoultra = 60;
+let tresa4 = 5;
+let maisque4 = 7.50;
+
+let tipodeassinatura = prompt("Digite o nome da assinatura que você deseja:")
+let quantitelas = prompt("Digite quantas telas você tera: ")
+if (tipodeassinatura == "básico"){
+    if (quantitelas>=3 && quantitelas<=4){
+        alert("O seu plano é R$ "+(valordobasico+tresa4))
+    } else if (quantitelas>=3 && quantitelas<=4){
+        alert("O seu plano será R$ "+(valordobasico+tresa4))
+    } else {
+        alert("O seu plano será R$ "+valordobasico)
+    }
+}
+
+
+else if (tipodeassinatura == "premium"){
+    if (quantitelas>=3 && quantitelas<=4){
+        alert("O seu plano é R$ "+(valordopremium+tresa4))
+    } else if (quantitelas>=3 && quantitelas>=4){
+        alert("O seu plano será R$ "+(valordopremium+tresa4))
+    } else {
+        alert("O seu plano será R$ "+(valordopremium))
+    }
+}
+
+
+else if (tipodeassinatura == "ultra"){
+    if (quantitelas>=3 && quantitelas<=4){
+        alert("O seu plano é R$ "+(valordoultra+tresa4))
+
+    } else if (quantitelas>=3 && quantitelas>=4){
+        alert("O seu plano será R$ "+(valordoultra+tresa4))
+    } else {
+        alert("O seu plano será R$ "+(valordoultra))
+    }
+}
+
+let daysOfWeek = ["Raquel", "Maraisa"];
+console.log(daysOfWeek[1])
+console.log(daysOfWeek.length)
+let nomedoaluno = prompt("Digite o nome do aluno: ")
+daysOfWeek.push(nomedoaluno)
+daysOfWeek.unshift("Miguel")
+daysOfWeek.pop()
+daysOfWeek.shift()
+daysOfWeek.sort()
+daysOfWeek.reverse()
+alert("Atualmente os alunos da escola são: "+daysOfWeek)
+alert(indexOfWeek.indexOf("Artur"))*/
